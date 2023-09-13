a82b637dcd1f19c0b647ab026f9c3ae4d2cc78b7
Michel <michellopez_16@hotmail.com>
2023-09-13 13:58:02 -0600

    first commit

---
+++ operaciones.py
@@ -0,0 +1,21 @@
+
+print("Inicio desde operaciones")
+
+def suma(a,b):
+    return a+b
+
+def resta(a,b):
+    return a-b
+
+
+def saluda():
+    print(tmp,mensaje)
+
+tmp = 10
+mensaje = "Hola mundo"
+
+
+
+print(tmp,mensaje)
+
+print("Fin desde operaciones")
