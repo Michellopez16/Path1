b28a8215bd997da9e14e9b6eea96ae5c011c3ce8
Michel <michellopez_16@hotmail.com>
2023-09-14 13:57:57 -0600

    Fin de path romba

--- Rombapath/main.py
+++ Rombapath/main.py
@@ -14,22 +14,66 @@ roomba_img = os.path.join(cwd,'roomba.png')
 
 class Roomba(pygame.sprite.Sprite):
     
-    def __init__(self) -> None:
+    def __init__(self,empty_path) -> None:
         super().__init__()
         self.image = pygame.image.load(roomba_img).convert_alpha()
         self.rect = self.image.get_rect(center = (60,60))
         
         #movement
         self.pos = self.rect.center 
-        self.speed = 0.6
+        self.speed = 3#2#0.6
         self.direction = pygame.math.Vector2(0,0)
         
         # path
         self.path = []
+        self.collision_rects = []
+        self.empty_path = empty_path
         
+    def get_coord(self):
         
+        col = self.rect.centerx // 32
+        row = self.rect.centery // 32
         
+        return (col,row)
         
+    def set_path(self, path):
+        self.path = path
+        self.create_collision_rects()
+        self.get_direction()
+        
+    def create_collision_rects(self):
+        if self.path:
+            self.collision_rects = []
+            for point in self.path:
+                x = (point[0] * 32) + 16
+                y = (point[1] * 32) + 16
+                rect = pygame.Rect( (x - 2,y -2), (4,4))
+                self.collision_rects.append(rect)
+                
+    
+    def get_direction(self):
+        if self.collision_rects:
+            start = pygame.math.Vector2(self.pos)
+            end = pygame.math.Vector2(self.collision_rects[0].center)
+            self.direction= (end - start).normalize()
+        else:
+            self.direction = pygame.math.Vector2(0,0)
+            self.path = []
+            
+    def check_collision(self):
+        if self.collision_rects:
+            for rect in self.collision_rects:
+                if rect.collidepoint(self.pos):
+                    del self.collision_rects[0]
+                    self.get_direction()
+        else:
+            self.empty_path()
+                    
+                    
+    def update(self):
+        self.pos += self.direction * self.speed
+        self.check_collision()
+        self.rect.center = self.pos
 
 class PathFinder:
     def __init__(self,matrix) -> None:
@@ -42,12 +86,16 @@ class PathFinder:
         self.path = []
         
         # Roomba
-        self.roomba = pygame.sprite.GroupSingle(Roomba())
+        self.roomba = pygame.sprite.GroupSingle(Roomba(self.empty_path))
         
+    def empty_path(self):
+        self.path = []
+        
+
 
     def create_path(self):
         #start
-        start_x,start_y = [1,1]
+        start_x,start_y = self.roomba.sprite.get_coord() #[1,1]
         start = self.grid.node(start_x,start_y)
         #end
         x,y = pygame.mouse.get_pos()
@@ -61,6 +109,7 @@ class PathFinder:
         
         self.grid.cleanup()
         #print(self.path)
+        self.roomba.sprite.set_path(self.path)
         
         
     def draw_path(self):
@@ -73,7 +122,7 @@ class PathFinder:
                 y = (point[1] * 32) +16
                 
                 points.append( ( x,y ) )
-                pygame.draw.circle(screen,'#4a4a4a',(x,y),2)
+                #pygame.draw.circle(screen,'#4a4a4a',(x,y),2)
             
             pygame.draw.lines(screen,color='#4a4a4a',closed=False,points=points,width=5)
 
